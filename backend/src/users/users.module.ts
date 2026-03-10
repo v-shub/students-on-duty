9e2d1cd9156947c6bceb7e456af3dd79fa91b889
v-shub <victoriya.shubina.mex@gmail.com>
2026-03-10 14:30:28 +0300

    verification service hotfix

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { VerificationService } from '../auth/verification.service';
 import { User } from './entities/user.entity';
 import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), AuthModule],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, VerificationService],
   exports: [UsersService],
 })
 export class UsersModule {}
